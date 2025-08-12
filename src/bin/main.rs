3fb3b58e5945d32b5a16ec5b6aebfae9830c2956
BadMachine <iskullbreakeri@gmail.com>
2025-08-13 01:19:38 +0200

    1.0.8

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,91 +8,38 @@ use napi::tokio_stream::StreamExt;
 async fn main() {
   // std::env::set_var("RUST_LOG", "hyper=trace,tonic=trace,h2=trace");
   // tracing_subscriber::fmt::init();
-
-  let mut client = influxdb3_napi::client::InfluxDBClient::new(
-        // String::from("http://165.232.154.186:8195"),
-        // Some(String::from("apiv3_64thndtOkGj3gpj5Mc3IwgSN9jKJ6c2Jle4-sJdQwsZ5nIThBjT9ALB0GjEpXvSgt2ZotiQzLbdtbFTEi8S2hg")),
-        String::from("http://165.232.154.186:8192"),
-        Some(String::from("apiv3_3UbvWHEjw_1Jo9qaz0DrH0g59mA_o6KxsDb8WTZI5Gd5PvzTOvD_UDocfMZ1yqnyvJmBd2Wrkkx8MXfVgcpDew")),
-        Some(influxdb3_napi::serializer::Serializer::Library),
-        None
-    );
-
-  let mut response = client
-    .query(QueryPayload {
-      database: "sample-air-sensor-1754383690707".to_string(),
-      query: "SELECT * FROM system.last_caches".to_string(),
-      _type: Some(QueryType::Sql),
-      params: None,
-    })
-    .await
-    .unwrap();
-
-  match response {
-    Either3::A(mut data) => match data.next().await {
-      Some(Ok(res)) => {
-        println!("result is {:?}", res)
-      }
-      _ => println!("no result found"),
-    },
-    _ => panic!(),
-  }
-
-  // for _ in 0..10 {
-  //     let start = Instant::now();
-  //     let mut response = client
-  //         .query(
-  //             String::from("_internal"),
-  //             String::from("SELECT * FROM system.databases"),
-  //             Some(QueryType::Sql),
   //
-  //         )
-  //         .await
-  //         .unwrap();
-  //     //
   //
-  //     match response.next().await {
-  //         Ok(res) => match res {
-  //             Some(data) => {
-  //                 // println!("result is {:?}", data);
-  //                 println!("Time elapsed: {:?}", start.elapsed());
-  //             }
-  //             None => {
-  //                 println!("Empty response");
-  //             }
-  //         },
-  //         Err(e) => {
-  //             println!("Error occurred while the request {}", e);
-  //         }
-  //     }
-  // }
-
-  // napi::tokio::time::sleep(Duration::from_secs(10)).await;
-  // let mut point = Point::from_measurement(String::from("test_measurement"));
   //
-  // let mut fields: HashMap<String, Either5<bool, f64, u32, i64, String>> = HashMap::new();
-  // fields.insert(String::from("A"), Either5::C(4));
-  // fields.insert(String::from("B"), Either5::B(3.));
   //
-  // point.set_fields(fields).expect("TODO: panic message");
+  // let mut client = influxdb3_napi::client::InfluxDBClient::new(
+  //       // String::from("http://165.232.154.186:8195"),
+  //       // Some(String::from("apiv3_64thndtOkGj3gpj5Mc3IwgSN9jKJ6c2Jle4-sJdQwsZ5nIThBjT9ALB0GjEpXvSgt2ZotiQzLbdtbFTEi8S2hg")),
+  //       String::from("http://165.232.154.186:8192"),
+  //       Some(String::from("apiv3_3UbvWHEjw_1Jo9qaz0DrH0g59mA_o6KxsDb8WTZI5Gd5PvzTOvD_UDocfMZ1yqnyvJmBd2Wrkkx8MXfVgcpDew")),
+  //       Some(influxdb3_napi::serializer::Serializer::Library),
+  //       None
+  //   );
   //
-  // println!("Full elapsed time: {:?}", start.elapsed());
-  //
-  // println!("Line protocol: {:?}", point.to_line_protocol(None, None));
-  //
-  // let mut write_options = WriteOptions::default();
-  // write_options.no_sync = Some(true);
-  //
-  // // client.write(vec![String::from("cpu,host=server01 usage=85.2 1638360000000")], String::from("test"), Some(write_options), None).await;
-  // client
-  //   .write(
-  //     vec![point.to_line_protocol(None, None).unwrap()],
-  //     String::from("test_line"),
-  //     Some(write_options),
-  //     None,
-  //   )
+  // let mut response = client
+  //   .query(QueryPayload {
+  //     database: "sample-air-sensor-1754383690707".to_string(),
+  //     query: "SELECT * FROM system.last_caches".to_string(),
+  //     _type: Some(QueryType::Sql),
+  //     params: None,
+  //   })
   //   .await
-  //   .expect("TODO: panic message");
+  //   .unwrap();
+  //
+  // match response {
+  //   Either3::A(mut data) => match data.next().await {
+  //     Some(Ok(res)) => {
+  //       println!("result is {:?}", res)
+  //     }
+  //     _ => println!("no result found"),
+  //   },
+  //   _ => panic!(),
+  // }
 }
 
 #[tokio::main]
